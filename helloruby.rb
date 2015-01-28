cdb0ed7b9ac6e3d65a41d0d646e7da2de95a316c
mrscappy <club4238@hotmail.com>
2015-01-28 23:23:35 +0000

    first ruby files

---
+++ helloruby.rb
@@ -0,0 +1,17 @@
+puts "Hello! What's your name?"
+name = gets.chomp
+puts "Hello you are " + name + ", Welcome to Ruby!"
+
+puts "Where do you live?"
+city = gets.chomp
+puts "I live in " + city + ", South Carolina!"
+
+puts "Do you have any animals?"
+number = gets.chomp
+puts "I have a combined " + number + " dogs & a cat"
+
+puts "What are your dogs and cats names?"
+name1 = gets.chomp
+name2 = gets.chomp
+name3 = gets.chomp
+puts " My dogs names are " + name1 + "," + name2 + " and " + name3 + "." 
